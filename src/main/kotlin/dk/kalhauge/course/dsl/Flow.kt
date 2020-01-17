80d517422eb6127565663d6f6edd7d56d6ced31e
eguahlak <anders@kalhauge.dk>
2020-01-17 17:08:00 +0100

    cleaning up

--- src/main/kotlin/dk/kalhauge/course/dsl/Flow.kt
+++
@@ -1,7 +0,0 @@
-package dk.kalhauge.course.dsl
-
-class Flow(val course: Course, override val title: String) : Target {
-  override val link: String
-    get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
-  }
-
--- src/main/kotlin/dk/kalhauge/course/dsl/General.kt
+++
@@ -1,7 +0,0 @@
-package dk.kalhauge.course.dsl
-
-interface Target {
-  val title: String
-  val link: String
-  }
-
