8cc53bb921b53fc5b87abc8a53429cfd86709a42
eguahlak <anders@kalhauge.dk>
2021-01-10 15:10:01 +0100

    version 2.2.1 - fixing courseRoot

--- src/main/kotlin/dk/kalhauge/document/dsl/Configuration.kt
+++ src/main/kotlin/dk/kalhauge/document/dsl/Configuration.kt
@@ -29,7 +29,7 @@ class Configuration(
       }
     this.contextRoot = courseRoot
         ?: properties["context.root"]
-        ?: CourseContext.root
+        ?: this::class.java.classLoader.getResource(".").path.substringBeforeLast("/dsl/")
     this.root = File(this.contextRoot)
     }
 
